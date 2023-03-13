b0d4cc95b6a50e5e51e512c3b42ae5ea0600cdb4
Himanshu <himamshuthakur20@gmail.com>
2023-03-14 00:24:49 +0530

    alternate swap

---
+++ OutsideLeet/alternateswap.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+
+using namespace std;
+
+void alternateswap(int arr[],int size){
+
+    int first,second;
+
+    for(int i=0;i<size;i+=2){
+
+        int temp=0;
+        first=i;
+        second=i+1;
+
+        // cout<<"first "<<first<<endl;
+        // cout<<"second "<<second<<endl;
+        if(first==size-1){
+            return;
+        }       
+
+        //swap
+
+        // cout<<"first1 "<<arr[first]<<endl;
+        // cout<<"second1 "<<arr[second]<<endl;
+
+        temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+
+        // cout<<"first2 "<<arr[first]<<endl;
+        // cout<<"second2 "<<arr[second]<<endl;
+        
+
+    }
+
+    //  for(int i=0;i<5;i++){
+
+    //     cout<<arr[i]<<" ";
+    // }
+
+    return;
+}
+
+int main(){
+
+    int arr[5];
+
+    cout<<"Enter the array elements\n";
+
+    for(int i=0;i<5;i++){
+
+        cin>>arr[i];
+    }
+
+    alternateswap(arr,5);
+
+    cout<<"Alternate swaped array:\n";
+
+    for(int i=0;i<5;i++){
+
+        cout<<arr[i]<<" ";
+    }
+
+    cout<<endl;
+
+    return 0;
+}
